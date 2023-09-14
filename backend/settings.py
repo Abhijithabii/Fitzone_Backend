b0c13ba0b37598a00548c2617fe0c8a031cf09f6
Abhijith <abhijithrajeevan123@gmail.com>
2023-09-14 18:26:44 +0530

    udpating setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -206,7 +206,7 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_TLS = True
 
 
 
